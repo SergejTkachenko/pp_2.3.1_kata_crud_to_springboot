872a7b2c6cde50e4e0110c5bc774f62735c216f4
SergejTkachenko <tks.emailbox@gmail.com>
2022-08-19 13:43:10 +0300

    v1.1 fix notices: 1. Сделал возможной работу с кириллицей 2. Использовал не @RequestMapping, а другие аннотации по типу: @PostMapping, @DeleteMapping 3. UserController: showAllUsers - возвращает String 4. addNewUser-view.html: поменял нейминг

--- src/main/java/com/tkc/katacrud/springboot/kata_crud_to_springboot/controller/UserController.java
+++ src/main/java/com/tkc/katacrud/springboot/kata_crud_to_springboot/controller/UserController.java
@@ -5,9 +5,7 @@ import com.tkc.katacrud.springboot.kata_crud_to_springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -22,39 +20,38 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping("/")
-    public ModelAndView showAllUsers(Model model) {
-        List<User> users = userService.getAllUsers();
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("main-view");
-        modelAndView.addObject("userList", users);
+    @GetMapping("/")
+    public String showAllUsers(Model model) {
 
-        return modelAndView;
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("userList", users);
+
+        return "main";
     }
 
-    @RequestMapping("/addNewUser")
+    @GetMapping("/addNewUser")
     public String addNewUser(Model model) {
         User user = new User();
         model.addAttribute("user", user);
-        return "addNewUser-view";
+        return "addUser";
     }
 
-    @RequestMapping("/saveUser")
+    @PostMapping("/saveUser")
     public String saveUser(@ModelAttribute("user") User user) {
         userService.saveUser(user);
 
         return "redirect:/";
     }
 
-    @RequestMapping( "/updateUser/{id}")
+    @PutMapping( "/updateUser/{id}")
     public String updateUser(@PathVariable("id") int id, Model model) {
         User user = userService.getUser(id);
         model.addAttribute("user", user);
 
-        return "updateUser-view";
+        return "updateUser";
     }
 
-    @RequestMapping("/deleteUser/{id}")
+    @DeleteMapping("/deleteUser/{id}")
     public String deleteUser(@PathVariable("id") int id) {
         User user = userService.getUser(id);
         userService.deleteUser(user);
